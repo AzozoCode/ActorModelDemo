b4256a0be9821115ed3d2d9b0c384d2a49f9ec09
AzozoCode <josephbarrigah@gmail.com>
2023-06-27 22:13:42 +0000

    initial commit

---
+++ ActorModelDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ActorModelDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

79df56f8a17e40201d16c16111b6f8c937916d4e
AzozoCode <josephbarrigah@gmail.com>
2023-06-28 10:45:37 +0000

    add a Reservation Actor class

--- ActorModelDemo/Program.cs
+++ ActorModelDemo/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ActorModelDemo
+﻿using Akka.Actor;
+
+namespace ActorModelDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+           
+
+            Console.ReadLine();
         }
     }
 }
---
+++ ActorModelDemo/ReservationActor.cs
@@ -0,0 +1,26 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActorModelDemo
+{
+    internal class ReservationActor:ReceiveActor
+    {
+        private readonly List<RoomState> _rooms = new List<RoomState>
+        {
+            new RoomState{ RoomNumber= 1,IsBook=false},
+            new RoomState{RoomNumber= 2,IsBook=false},
+            new RoomState{RoomNumber= 3,IsBook=true},
+            new RoomState{RoomNumber=4,IsBook=true}
+
+        };
+
+        public ReservationActor()
+        {
+            Receive<BookTheRoom>();
+        }
+    }
+}
